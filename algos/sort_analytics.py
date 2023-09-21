aa477e3d1d38905da5da9901068ee92d7722e583
nmgetahun <nmgetahun@gmail.com>
2023-09-21 12:19:07 -0500

    Added comments

--- algos/sort_analytics.py
+++ algos/sort_analytics.py
@@ -2,19 +2,20 @@ from __future__ import annotations
 from typing import List, Callable
 from time import perf_counter
 
-DEFAULT_LIST = [1, 4, 2, 9, 10, 8, 19, 11, 5, 100, 7, 6, 0, 10, 10, 10]
+DEFAULT_UNSORTED_LIST = [1, 4, 2, 9, 10, 8, 19, 11, 5, 100, 7, 6, 0, 10, 10, 10]
 
 def test_versions(versions: List[Callable], lst: List[int]=None, prnt: bool=False) -> None:
+    """Test validity of any # of implementations of a sorting algo"""
     if not lst:
-        lst = DEFAULT_LIST[:]
+        lst = DEFAULT_UNSORTED_LIST[:]
 
     print("Testing versions")
     sorted_lst = sorted(lst)
 
     for version in versions:
-        test_lst = lst[:]
+        test_lst = lst[:] # assume test_lst will be sorted in place
         return_val = version(test_lst, prnt)
-        if return_val:
+        if return_val: # for when the version returns a sorted copy
             test_lst = return_val
 
         valid_sort = ("Invalid", "Valid")[test_lst == sorted_lst]
@@ -24,14 +25,15 @@ def test_versions(versions: List[Callable], lst: List[int]=None, prnt: bool=Fals
 
 
 def time_versions(versions: List[Callable], lst: List[int]=None) -> None:
+    """Time any # of implementations of a sorting algo"""
     if not lst:
-        lst = DEFAULT_LIST[:]
+        lst = DEFAULT_UNSORTED_LIST[:]
 
     print("Timing versions")
 
     for version in versions:
         start = perf_counter()
-        for _ in range(1000):
+        for _ in range(1000): # might make this a parameter
             version(lst[:], False)
         end = perf_counter()
 
